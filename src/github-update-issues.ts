6e9a7cdc0cf8d7f7f8f64d4c7b5c42e4665c850b
IDCs <idc.dts@gmail.com>
2024-10-29 08:43:27 +0000

    more cleanup

--- src/github-update-issues.ts
+++ src/github-update-issues.ts
@@ -12,13 +12,47 @@ async function run() {
     const repo = 'nexus-mods/vortex';
     const outputDir = path.resolve(__dirname, path.join('..', 'out'));
     const issuesReportFile = path.join(outputDir, 'issues_report.json');
-
     // Ensure output directory exists
     if (!fs.existsSync(outputDir)) {
       fs.mkdirSync(outputDir, { recursive: true });
     }
-
-    const genIssue = (data: any): IGithubIssue => data as IGithubIssue;
+    try {
+      fs.unlinkSync(issuesReportFile);
+    } catch (err) {
+      // do nothing
+    }
+    const mapToGithubIssue = (rawIssue: any): IGithubIssue => {
+      return {
+        url: rawIssue.url,
+        repository_url: rawIssue.repository_url,
+        id: rawIssue.id,
+        title: rawIssue.title,
+        user: rawIssue.user,
+        labels: rawIssue.labels.map((label: any) => ({
+          id: label.id,
+          node_id: label.node_id,
+          url: label.url,
+          name: label.name,
+          color: label.color,
+          default: label.default,
+          description: label.description
+        })),
+        state: rawIssue.state,
+        locked: rawIssue.locked,
+        created_at: rawIssue.created_at,
+        updated_at: rawIssue.updated_at,
+        closed_at: rawIssue.closed_at,
+        body: rawIssue.body,
+        closed_by: rawIssue.closed_by,
+        state_reason: rawIssue.state_reason,
+        pull_request: rawIssue.pull_request ? {
+          url: rawIssue.pull_request.url,
+          html_url: rawIssue.pull_request.html_url,
+          diff_url: rawIssue.pull_request.diff_url,
+          patch_url: rawIssue.pull_request.patch_url
+        } : undefined
+      };
+    }
     // Function to fetch issues with pagination support
     const fetchIssues = async (state: string, since: string | null) => {
       let issues: IGithubIssue[] = [];
@@ -34,8 +68,13 @@ async function run() {
         });
 
         // Remove PRs
-        issues = response.data.filter((issue: IGithubIssue) => !issue.pull_request && !issue.title.toLowerCase().startsWith('review'));
-        issues = issues.map(genIssue);
+        issues = response.data.reduce((acc: IGithubIssue[], issue: IGithubIssue) => {
+          if (!issue || issue.pull_request || issue.title.toLowerCase().startsWith('review')) {
+            return acc;
+          }
+          acc.push(mapToGithubIssue(issue));
+          return acc;
+        }, []);
 
         if (response.data.length < 100) {
           hasMorePages = false; // If fewer than 100 issues are returned, we're done
